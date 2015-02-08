9b0bc1b9d56121058f2ad29ea92e74b27864f76e
Anthony Candaele <anthony.candaele@webmove.be>
2015-02-08 12:15:22 +0100

    implement signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'news', to: 'news_articles#index'
   get 'news/article/:id', to: 'news_articles#show', as: 'news_article'
-  
+  get 'signin', to: 'sessions#new' 
   get 'admin', to: 'dashboard#index'
+
+  resource :session
 
   namespace :admin do
     resources :news_articles
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208085628) do
+ActiveRecord::Schema.define(version: 20150208110756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20150208085628) do
     t.string   "link"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end

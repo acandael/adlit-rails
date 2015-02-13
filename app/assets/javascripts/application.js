13d6d88ea3edc9ff2a1feeef9daf05362debcfb4
Anthony Candaele <anthony.candaele@webmove.be>
2015-02-13 09:38:53 +0100

    reimplement fullPage.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.onepage-scroll
+//= require jquery.fullPage
 //= require turbolinks
 //= require_tree .
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
+
   def index
+    @disable_footer = true
     @news_articles = NewsArticle.all
   end
 end

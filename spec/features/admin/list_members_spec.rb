fcd504a6779bd2ec5df87e8c620f530dd11d0669
acandael <anthony.candaele@ugent.be>
2015-03-26 09:35:02 +0100

    change test to show members in order of position

--- spec/features/admin/list_members_spec.rb
+++ spec/features/admin/list_members_spec.rb
@@ -4,11 +4,11 @@ describe "Viewing the list of members" do
 
   let(:user) { User.create!(user_attributes) }
 
-  let!(:member1) { Member.create!(member_attributes(name: "Pieter De Pauw Panic", email: "pieter.depauw@ugent.be")) }
+  let!(:member1) { Member.create!(member_attributes(name: "Pieter De Pauw Panic", email: "pieter.depauw@ugent.be", position: 3)) }
 
-  let!(:member2) { Member.create!(member_attributes(name: "Katarina Panic", email: "katarina.panic@ugent.be")) }
+  let!(:member2) { Member.create!(member_attributes(name: "Katarina Panic", email: "katarina.panic@ugent.be", position: 1)) }
 
-  let!(:member3) { Member.create!(member_attributes(name: "Liselot Hudders")) }
+  let!(:member3) { Member.create!(member_attributes(name: "Liselot Hudders", position: 2)) }
 
 
   before do
@@ -22,8 +22,8 @@ describe "Viewing the list of members" do
     expect(page).to have_text(member3.name)
   end
 
-  it "shows the members in alfabetical order" do
-    expect(Member.by_name).to eq([member2, member3, member1])
+  it "shows the members in order by position" do
+    expect(Member.order_by_position).to eq([member2, member3, member1])
   end
 end
 

3cd3bdae0c2de29516d95320ecbd5be05d0ab517
acandael <anthony.candaele@ugent.be>
2015-09-11 10:26:54 +0200

    reset pagination back to 10 pages

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -1,10 +1,10 @@
 class NewsArticlesController < ApplicationController
   def index
     if (category_selected?)
-      @news_articles = NewsArticle.filter_by_category(params[:news_article][:news_category]).page(params[:page]).per(2)
+      @news_articles = NewsArticle.filter_by_category(params[:news_article][:news_category]).page(params[:page]).per(10)
       @selected = params[:news_article].try(:[], :news_category)
     else
-      @news_articles = NewsArticle.order(created_at: :desc).page(params[:page]).per(2)
+      @news_articles = NewsArticle.order(created_at: :desc).page(params[:page]).per(10)
     end
   end
 

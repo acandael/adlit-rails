3422131c17c06d2b379a7d811dc76d88393d25cc
acandael <anthony.candaele@ugent.be>
2016-04-28 10:11:33 +0200

    fix N+1 query by adding includes(:news_category)

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -4,7 +4,7 @@ class NewsArticlesController < ApplicationController
       @news_articles = NewsArticle.filter_by_category(params[:news_article][:news_category]).page(params[:page]).per(10)
       @selected = params[:news_article].try(:[], :news_category)
     else
-      @news_articles = NewsArticle.order(created_at: :desc).page(params[:page]).per(10)
+      @news_articles = NewsArticle.order(created_at: :desc).includes(:news_category).page(params[:page]).per(10)
     end
   end
 
